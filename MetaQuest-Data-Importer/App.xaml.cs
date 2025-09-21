937f251cde0ef54b6f52cea20e4b3c8f588b18a1
cjsiador <54792076+cjsiador@users.noreply.github.com>
2025-09-20 20:17:30 -1000

    Upload .net maui

---
+++ MetaQuest-Data-Importer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MetaQuest_Data_Importer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MetaQuest-Data-Importer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MetaQuest_Data_Importer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
